e0d757ec4e067583e8da8d5b022ef1eab53bb446
User <vargas.mattykun@gmail.com>
2026-04-07 17:31:53 +0800

    Dark mode: default

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -4,6 +4,6 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light">{children}</NextThemesProvider>
+    <NextThemesProvider attribute="class" defaultTheme="dark">{children}</NextThemesProvider>
   );
 }
